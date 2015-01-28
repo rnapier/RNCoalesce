a48cb4da580c4d05bc794efdc89691f800d5e89d
Rob Napier <robnapier@gmail.com>
2015-01-28 08:54:47 -0500

    Initial Commit

---
+++ RNCoalesce/RNCoalesce.h
@@ -0,0 +1,13 @@
+//
+//  RNCoalesce.h
+//  RNCoalesce
+//
+//  Created by Rob Napier on 1/28/15.
+//  Copyright (c) 2015 Rob Napier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCoalesce : NSObject
+
+@end
---
+++ RNCoalesce/RNCoalesce.m
@@ -0,0 +1,13 @@
+//
+//  RNCoalesce.m
+//  RNCoalesce
+//
+//  Created by Rob Napier on 1/28/15.
+//  Copyright (c) 2015 Rob Napier. All rights reserved.
+//
+
+#import "RNCoalesce.h"
+
+@implementation RNCoalesce
+
+@end
